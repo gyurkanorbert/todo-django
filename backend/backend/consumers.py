f5e14c194d5c21fe8664536d26c3b67aa5dc4992
Norbert <gyurkanorbert@gmail.com>
2024-12-05 21:20:00 +0200

    finalized project

--- backend/backend/consumers.py
+++ backend/backend/consumers.py
@@ -25,8 +25,8 @@ class GroupConsumer(WebsocketConsumer):
 class TodoGroupConsumer(WebsocketConsumer):
     def connect(self):
         self.channel_layer = get_channel_layer()
-        self.group_id = self.scope['url_route']['kwargs']['group_id']
-        self.group_name = f"group_{self.group_id}" # lekeri a matchelo url routebol a csoportid-t
+        self.group_id = self.scope['url_route']['kwargs']['group_id'] # lekeri a matchelo url routebol a csoportid-t
+        self.group_name = f"group_{self.group_id}"
         async_to_sync(self.channel_layer.group_add)(
                             self.group_name,
                             self.channel_name
@@ -41,14 +41,14 @@ class TodoGroupConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         print(f"Received message type: {text_data_json.get('type')}")
 
-        if text_data_json.get('type') == "todo_event":
-            async_to_sync(self.channel_layer.group_send)(
-                self.group_name,
-                {
-                    'type': 'todo_event',
-                    'todo': text_data_json['todo']
-                })
-        elif text_data_json.get('type') == "todo_change_status":
+        # if text_data_json.get('type') == "todo_event":
+        #     async_to_sync(self.channel_layer.group_send)(
+        #         self.group_name,
+        #         {
+        #             'type': 'todo_event',
+        #             'todo': text_data_json['todo']
+        #         })
+        if text_data_json.get('type') == "todo_change_status":
             todo_id = text_data_json.get('todo_id')
 
             todo = Todo.objects.get(uuid=todo_id)
